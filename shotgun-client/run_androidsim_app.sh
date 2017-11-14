018129691b6cc1025faa1ea113fc49999101e749
Gbemiga Meduoye <Gbemiga@claymoreminds.com>
2017-11-14 11:10:09 +0000

    Fixed run anroid sim shell script to start simulator

--- shotgun-client/run_androidsim_app.sh
+++ shotgun-client/run_androidsim_app.sh
@@ -1,2 +1,13 @@
+EMULATOR=$1
+
+if [ -n "$EMULATOR" ]; then
+    echo "Starting $EMULATOR"
+	emulator -avd $EMULATOR &
+	sleep 10
+else
+	echo "No AVD specified please specify AVD from list below"
+	emulator -list-avds
+fi
+
 adb reverse tcp:7007 tcp:7007
 react-native run-android
