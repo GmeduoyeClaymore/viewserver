61ad245d23bbfad70b250d83a39d00dd23718f71
Gbemiga Meduoye <Gbemiga@claymoreminds.com>
2017-12-13 13:37:41 +0000

    Can now specify action params

--- viewserver-core/src/main/java/io/viewserver/command/ControllerActionEntry.java
+++ viewserver-core/src/main/java/io/viewserver/command/ControllerActionEntry.java
@@ -1,26 +1,33 @@
 package io.viewserver.command;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ControllerActionEntry{
 
     private final Class<?> parameterType;
     private final Class<?> returnType;
+    private final List<ControllerParamEntry> actionParams;
     private Method method;
     private Object controller;
     private static ObjectMapper mapper = new ObjectMapper();
     private ControllerAction an;
+    private static TypeReference<HashMap<String,String>> dictionaryType = new TypeReference<HashMap<String,String>>() {};
 
     public ControllerActionEntry(Method method, Object controller, ControllerAction an, Controller controllerAttribute) {
         this.an = an;
-        if(method.getParameterTypes().length > 1){
-            throw new RuntimeException("Problem with constructing controller action \"" + an.path() + "\" on controller \"" + controllerAttribute.name() + "\". All actions must have a single parameter at most");
-        }
+        this.actionParams =  getActionParams(method);
         this.parameterType = method.getParameterTypes().length == 1 ? method.getParameterTypes()[0] : null;
         this.returnType = method.getReturnType();
 
@@ -35,6 +42,29 @@ public class ControllerActionEntry{
         this.controller = controller;
     }
 
+    private List<ControllerParamEntry> getActionParams(Method method) {
+        List<ControllerParamEntry> result  = new ArrayList<>();
+        List<String> errors = new ArrayList<>();
+        for(int i=0;i<method.getParameterTypes().length;i++){
+            Annotation[] annotations = method.getParameterAnnotations()[i];
+            for (Annotation an : annotations){
+                if(an instanceof ActionParam){
+                    Class<?>[] parameterTypes = method.getParameterTypes();
+                    result.add(new ControllerParamEntry(((ActionParam) an).name(),parameterTypes[i],i));
+                    continue;
+                }
+                errors.add(String.format("Parameter index %s does not contain an action parameter",i));
+            }
+        }
+        if(errors.size() > 0){
+            if(method.getParameterTypes().length > 1){
+                throw new RuntimeException("Some parameters didnt have an action param attribute. This will only fly if you have single parameter methods\n" + String.join(",",errors));
+            }
+            return null;
+        }
+        return result;
+    }
+
     public boolean isSynchronous(){
         return an.isSynchronous();
     }
@@ -42,7 +72,34 @@ public class ControllerActionEntry{
     public String invoke(String param){
         Object arg = fromString(param, this.parameterType);
         try {
-            Object result = this.parameterType == null ? method.invoke(this.controller) : method.invoke(this.controller, arg);
+            Object result;
+            if(this.actionParams != null && actionParams.size() > 0){
+                HashMap<String,String> map = mapper.readValue(param, dictionaryType);
+                Object[] args = new Object[this.actionParams.size()];
+                List<String> errors = new ArrayList<>();
+                for(ControllerParamEntry paramEntry : this.actionParams){
+                    String parameter = map.get(paramEntry.getName());
+                    if(parameter == null){
+                        errors.add(String.format("Unable to find parameter named \"%s\" in argument map \"%s\"",paramEntry.getName(),param));
+                    }else{
+                        try{
+                            args[paramEntry.index] = mapper.readValue(parameter,paramEntry.getType());
+                        }
+                        catch (Exception ex){
+                            errors.add(ex.getMessage());
+                        }
+                    }
+                }
+                if(errors.size() > 0){
+                    throw new RuntimeException(String.format("Problems invoking method params \"%s\"", String.join(",",errors)));
+                }
+                result = method.invoke(this.controller, args);
+            }
+            else{
+                result = this.parameterType == null ? method.invoke(this.controller) : method.invoke(this.controller, arg);
+            }
+
+
             if(this.returnType == null){
                 return null;
             }
@@ -51,6 +108,12 @@ public class ControllerActionEntry{
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
             throw new RuntimeException(e.getCause());
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonParseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
