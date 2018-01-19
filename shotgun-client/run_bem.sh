ee1bc25277a5ed47ae42d930d900e5c497e010ef
Gbemiga Meduoye <Gbemiga@claymoreminds.com>
2018-01-19 14:47:04 +0000

    Pushed script changes

--- shotgun-client/run_bem.sh
+++ shotgun-client/run_bem.sh
@@ -1,2 +1,2 @@
 export ANDROID_NDK=C:\\git\\android-ndk-r10e-windows-x86_64\\android-ndk-r10e
-./run_androidsim_app.sh -e Nexus_5X2+Nexus_5X3_2 -p 5565+5567
+./run_androidsim_app.sh -e CUSTOMER_DEVICE+DRIVER_DEVICE -p 5565+5567
