59fc65819fab124c92178b52c8af52c7a3827074
Gbemiga Meduoye <Gbemiga@claymoreminds.com>
2018-02-01 11:57:31 +0000

    Added ASAP to date picker

--- shotgun-client/app/common/components/datePicker/DatePicker.js
+++ shotgun-client/app/common/components/datePicker/DatePicker.js
@@ -1,7 +1,43 @@
-import { Platform } from 'react-native';
+import { Platform, View } from 'react-native';
 import CustomDatePickerAndroid from './CustomDatePickerAndroid';
 import CustomDatePickerIOS from './CustomDatePickerIOS';
-
+import React, {Component} from 'react';
+import moment from 'moment';
+import {Button, Text} from 'native-base';
 const IS_ANDROID = Platform.OS === 'android';
 
-export default (IS_ANDROID ? CustomDatePickerAndroid : CustomDatePickerIOS);
+export const DatePicker =  (IS_ANDROID ? CustomDatePickerAndroid : CustomDatePickerIOS);
+
+export default class DatePickerControl extends Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.setDate = this.setDate.bind(this);
+    this.setAsap = this.setAsap.bind(this);
+  }
+
+  setDate(date){
+    this.setState({date});
+  }
+
+  setAsap(){
+    const {onConfirm} = this.props;
+    const asapDate = moment().add(30, 'minute').toDate();
+    this.setDate();
+    if (onConfirm){
+      onConfirm(asapDate);
+    }
+  }
+
+  render(){
+    const {props, state} = this;
+    const {date} = state;
+    const newProps = {...props, date};
+    return <View>
+      <DatePicker {...newProps}/>
+      <Button onPress={this.setAsap} style={{height: 18, marginLeft: 15}}>
+        <Text>ASAP</Text>
+      </Button>
+    </View>;
+  }
+}
