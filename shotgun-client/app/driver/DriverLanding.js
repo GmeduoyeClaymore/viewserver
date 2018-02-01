133560f35f79c8a81a2852f3231b7592079cfca3
Gbemiga Meduoye <Gbemiga@claymoreminds.com>
2018-02-01 17:21:34 +0000

    DOB now in driver registration

--- shotgun-client/app/driver/DriverLanding.js
+++ shotgun-client/app/driver/DriverLanding.js
@@ -57,7 +57,7 @@ class DriverLanding extends Component {
           <Route path={'/Driver/DriverOrderDetail'} exact render={() => <DriverOrderDetail client={client} {...this.props}/>}/>
           <Route path={'/Driver/DriverOrderInProgress'} exact render={() => <DriverOrderInProgress client={client} {...this.props}/>}/>
           <Route path={'/Driver/Settings'} render={() => <DriverSettings client={client} {...this.props}/>}/>
-          <Redirect to={'/Driver/DriverOrders'}/>
+          <Redirect to={'/Driver/DriverOrderRequests'}/>
         </Switch>
         <DriverMenuBar/>
       </Container>;
