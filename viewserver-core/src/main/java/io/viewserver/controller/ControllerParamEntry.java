d55d047513a9a9b8046a254ce262bc4fdd0ffe79
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-03-08 16:24:56 +0000

    Fixed view server grid

---
+++ viewserver-core/src/main/java/io/viewserver/controller/ControllerParamEntry.java
@@ -0,0 +1,47 @@
+package io.viewserver.controller;
+
+public class ControllerParamEntry{
+    String name;
+    Class type;
+    int index;
+    private boolean required;
+
+    public ControllerParamEntry(String name, Class type, int index, boolean required) {
+        this.name = name;
+        this.type = type;
+        this.index = index;
+        this.required = required;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Class getType() {
+        return type;
+    }
+
+    public void setType(Class type) {
+        this.type = type;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
---
+++ viewserver-core/src/main/java/io/viewserver/controller/ControllerRegistration.java
@@ -0,0 +1,60 @@
+package io.viewserver.controller;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerRegistration{
+
+    private final Controller controller;
+    private final HashMap<String,ControllerActionEntry> actions;
+
+    public ControllerRegistration(Object controller){
+        if(controller == null){
+            throw new RuntimeException("Controller must be specified");
+        }
+       this.controller = getControllerAnnotation(controller);
+        try{
+            this.actions = getActions(controller);
+        }catch (Exception ex){
+            throw new RuntimeException(String.format("Problem constructing controller \"%s\"",this.controller.name()));
+        }
+    }
+
+    public String getName(){
+        return this.controller.name();
+    }
+
+    public Map<String,ControllerActionEntry> getActions(){
+        return this.actions;
+    }
+
+    private HashMap<String, ControllerActionEntry> getActions(Object controller) {
+        Method[] methods = controller.getClass().getDeclaredMethods();
+        final HashMap<String,ControllerActionEntry> result = new HashMap<String,ControllerActionEntry>();
+
+        for(Method m : methods)
+        {
+            for(Annotation a : m.getAnnotations())
+            {
+                if ( a instanceof ControllerAction)
+                {
+                    ControllerAction an = (ControllerAction)a;
+                    result.put(an.path(),new ControllerActionEntry(m,controller,an,this.controller));
+                }
+            }
+        }
+        return result;
+    }
+
+
+    Controller getControllerAnnotation(Object controller){
+        for(Annotation a : controller.getClass().getAnnotations()){
+            if(a instanceof Controller){
+                return (Controller)a;
+            }
+        }
+        throw new RuntimeException("Invalid controller registration. Cannot find Controller parameter on type " + controller);
+    }
+}
