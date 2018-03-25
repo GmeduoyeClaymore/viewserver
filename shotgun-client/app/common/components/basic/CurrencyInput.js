4c198e93186c9ee77b38f1b20af452671630cc2d
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-03-25 13:51:20 +0100

    xxxx

---
+++ shotgun-client/app/common/components/basic/CurrencyInput.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { TextInput } from 'react-native';
+
+export const formatPrice = (price) => {
+  if (!price || price === 'undefined'){
+    return undefined;
+  }
+  return `£${(parseFloat(price + '')).toFixed(2)}`;
+};
+
+export class CurrencyInput extends Component{
+  constructor(props){
+    super(props);
+
+    this.setFormattedPriceValue = this.setFormattedPriceValue.bind(this);
+    this.clearFormattedPriceValue = this.clearFormattedPriceValue.bind(this);
+    this.setFormattedPriceValueFromProps = this.setFormattedPriceValueFromProps.bind(this);
+    this.onValueChanged = this.onValueChanged.bind(this);
+    this.state = {
+      formattedPrice: undefined
+    };
+  }
+
+  componentDidMount(){
+    this.setFormattedPriceValueFromProps(this.props);
+  }
+
+  componentWillReceiveProps(newProps){
+    this.setFormattedPriceValueFromProps(newProps);
+  }
+  setFormattedPriceValueFromProps(newProps){
+    const {initialPrice} = newProps;
+    const formattedPrice = formatPrice(initialPrice / 100);
+    this.setState({formattedPrice});
+  }
+
+  setFormattedPriceValue(){
+    const { onValueChanged } = this.props;
+    const {price } = this.state;
+    const formattedPrice = formatPrice(price);
+    if (onValueChanged){
+      onValueChanged(parseFloat(price) * 100);
+    }
+    this.setState({formattedPrice});
+  }
+
+  clearFormattedPriceValue(){
+    this.setState({formattedPrice: undefined});
+  }
+
+  onValueChanged(t){
+    this.setState({price: t});
+  }
+
+  render(){
+    const {formattedPrice} = this.state;
+    return <TextInput
+      {...this.props}
+      value={formattedPrice}
+      onFocus={this.clearFormattedPriceValue}
+      onBlur={this.setFormattedPriceValue}
+      onChangeText={this.onValueChanged}
+    />;
+  }
+}
