e69019cf1b0e08a2eaf6a4e115eb68388b761a17
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-03-26 04:53:50 +0100

    ADDED

---
+++ shotgun-client/app/customer/checkout/CheckoutUtils.js
@@ -0,0 +1,8 @@
+export const calculateTotalPrice = async ({client, orderItem, delivery }) => {
+  return await client.invokeJSONCommand('orderController', 'calculateTotalPrice', {orderItems: [stripImageData(orderItem)], delivery}).timeoutWithError(10000, 'Unable to load total price after 10 seconds');
+};
+
+const stripImageData = (item) => {
+  const {imageData, ...rest} = item;
+  return rest;
+};
