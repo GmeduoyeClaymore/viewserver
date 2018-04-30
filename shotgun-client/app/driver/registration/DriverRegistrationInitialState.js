30411f9db249a126ec37cf971648accd1c5d5ef0
Paul Graves <paulgraves@claymoreminds.com>
2018-04-30 10:34:45 +0100

    Reset registration inital state to empty

--- shotgun-client/app/driver/registration/DriverRegistrationInitialState.js
+++ shotgun-client/app/driver/registration/DriverRegistrationInitialState.js
@@ -1,4 +1,4 @@
-/*export const INITIAL_STATE = {
+export const INITIAL_STATE = {
   user: {
     firstName: undefined,
     lastName: undefined,
@@ -29,9 +29,9 @@
       weight: undefined
     }
   }
-};*/
+};
 
-export const INITIAL_STATE = {
+/*export const INITIAL_STATE = {
   user: {
     firstName: 'paul',
     lastName: 'graves',
@@ -66,5 +66,5 @@ export const INITIAL_STATE = {
       weight: undefined
     }
   }
-};
+};*/
 
