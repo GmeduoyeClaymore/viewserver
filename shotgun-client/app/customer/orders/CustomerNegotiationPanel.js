da8296c6d068ebf790d2018c76b047d305e7538b
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-05-04 20:31:25 +0100

    Added missing files

---
+++ shotgun-client/app/customer/orders/CustomerNegotiationPanel.js
@@ -0,0 +1,175 @@
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+import {Text, Grid, Row, Spinner, Col} from 'native-base';
+import {Icon, SpinnerButton, CurrencyInput, Currency} from 'common/components';
+import * as ContentTypes from 'common/constants/ContentTypes';
+import moment from 'moment';
+import {rejectResponse, acceptResponse, updateOrderAmount} from 'customer/actions/CustomerActions';
+import shotgun from 'native-base-theme/variables/shotgun';
+
+export default class OrderNegotiationPanel extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: undefined,
+      showAll: false
+    };
+  }
+
+  updateAmountInState = (amount) => {
+    super.setState({amount});
+  }
+
+  toggleShow = () => {
+    const {showAll} = this.state;
+    super.setState({showAll: !showAll});
+  }
+
+  clearAmount = () => {
+    this.updateAmountInState(undefined);
+    if (this.amountInput){
+      this.amountInput.clear();
+    }
+  }
+
+  render(){
+    const { order, partnerResponses, busyUpdating, dispatch} = this.props;
+    if (!order){
+      return null;
+    }
+    const canUpdateOrderPrice = !!~CAN_UPDATE_AMOUNT_STATUSES.indexOf(order.orderStatus);
+    return [<Grid  key="1"  style={{paddingTop: 10, marginBottom: 10}}>
+      <Col size={32}>
+        <Text style={{...styles.heading, marginTop: 10}}>{canUpdateOrderPrice ? 'Advertised Rate' : 'Agreed Price'}</Text>
+        <Row style={styles.row}>{!order.amount ? <Spinner/> : <Currency value={order.amount} style={styles.price} suffix={order.paymentType == 'DayRate' ?  ' a day' : ''}/>}</Row>
+        { canUpdateOrderPrice ? <CurrencyInput ref={ip => {this.amountInput = ip;}} dispatch={dispatch} onValueChange={this.updateAmountInState} placeholder="Enter updated amount"/> : null}
+      </Col>
+      {this.state.amount && canUpdateOrderPrice ? <Col size={20}>
+        <UpdateOrderPrice style={{marginTop: 17}} orderContentTypeId={order.orderContentTypeId} onAmountUpdated={this.clearAmount} dispatch={dispatch} orderId={order.orderId} amount={this.state.amount}/>
+      </Col> : null}
+    </Grid>,
+    <PartnerAcceptRejectControl   key="2" showAll={this.state.showAll}  dispatch={dispatch} orderId={order.orderId} orderStatus={order.orderStatus} orderContentTypeId={order.orderContentTypeId} partnerResponses={partnerResponses} busyUpdating={busyUpdating}/>,
+    partnerResponses.filter( res => !~ACTIVE_NEGOTIATION_STATUSES.indexOf(res.responseStatus)).length ? <Row  key="3" onPress={this.toggleShow}><Text>{this.state.showAll ? 'Hide rejected' : 'Show rejected'}</Text></Row> : null];
+  }
+}
+
+const ACTIVE_NEGOTIATION_STATUSES = ['RESPONDED', 'ACCEPTED'];
+const CAN_UPDATE_AMOUNT_STATUSES = ['PLACED'];
+const rejectedImageStyle = {
+  borderColor: shotgun.brandDanger,
+  borderWidth: 4
+};
+const respondedImageStyle = {
+  borderColor: shotgun.brandWarning,
+  borderWidth: 4
+};
+
+const acceptedImageStyle = {
+  borderColor: shotgun.brandSuccess,
+  borderWidth: 4
+};
+
+const ResponseStatusStyles = {
+  RESPONDED: respondedImageStyle,
+  ACCEPTED: acceptedImageStyle,
+  REJECTED: rejectedImageStyle
+};
+
+
+const  RejectPartner = ({orderId, partnerId, orderContentTypeId, busyUpdating, dispatch, style = {}, ...rest}) => {
+  const onRejectPartner = () => {
+    dispatch(rejectResponse({orderId, partnerId, orderContentTypeId}));
+  };
+  return <SpinnerButton {...rest} disabledStyle={{opacity: 0.1}} style={{...style, alignSelf: 'flex-start', flex: 1, marginRight: 0, marginLeft: 0}}  padded busy={busyUpdating} fullWidth danger onPress={onRejectPartner}><Text uppercase={false}>Reject</Text></SpinnerButton>;
+};
+
+const  AcceptPartner = ({orderId, partnerId, orderContentTypeId, busyUpdating, dispatch, style = {}, ...rest}) => {
+  const onAcceptPartner = () => {
+    dispatch(acceptResponse({orderId, partnerId, orderContentTypeId}));
+  };
+  return <SpinnerButton  {...rest} disabledStyle={{opacity: 0.1}} style={{...style, alignSelf: 'flex-start', flex: 1, marginRight: 0, marginLeft: 0}} padded busy={busyUpdating} fullWidth accept onPress={onAcceptPartner}><Text uppercase={false}>Accept</Text></SpinnerButton>;
+};
+
+const  UpdateOrderPrice = ({orderId, orderContentTypeId, amount, onAmountUpdated, busyUpdating, dispatch, style = {},  ...rest}) => {
+  const onUpdateOrderAmount = () => {
+    dispatch(updateOrderAmount({orderId, orderContentTypeId, amount}, onAmountUpdated));
+  };
+  return <SpinnerButton {...rest} padded busy={busyUpdating} style={{...style, alignSelf: 'flex-end', flex: 1, maxHeight: 45,  marginRight: 0, marginLeft: 0}}  fullWidth success onPress={onUpdateOrderAmount}><Text uppercase={false}>Update</Text></SpinnerButton>;
+};
+
+const PartnerAcceptRejectControl = ({partnerResponses = [], orderId, orderStatus, orderContentTypeId, busyUpdating, dispatch, showAll}) => {
+  return <Col>{partnerResponses.filter( res => showAll || !!~ACTIVE_NEGOTIATION_STATUSES.indexOf(res.responseStatus)).map(
+    (response, idx)  => {
+      const {partnerId, latitude, longitude, firstname, lastname, email, imageUrl, online, userStatus, statusMessage, ratingAvg, estimatedDate, price, responseStatus} = response;
+      const canRespond = !!~CAN_UPDATE_AMOUNT_STATUSES.indexOf(orderStatus) && responseStatus === 'RESPONDED';
+      const stars = [...Array(ratingAvg)].map((e, i) => <Icon name='star' key={i} style={styles.star}/>);
+      const imageStyle = styles.partnerImage;
+      const statusStyle = ResponseStatusStyles[responseStatus] || ResponseStatusStyles.REJECTED;
+      return <Row key={idx} style={{...styles.view, marginBottom: 10}}>
+        <Image style={{...imageStyle, ...statusStyle}}  resizeMode='stretch' source={{uri: imageUrl}}/>
+        <Col  size={50} style={{marginLeft: 10}}>
+          <Text style={{...styles.subHeading, marginBottom: 5}}>{firstname + ' ' + lastname}</Text>
+          {!!~ratingAvg ? <Row style={{marginBottom: 8, marginTop: 8}}>{stars}</Row> : <Text>No Ratings</Text>}
+          <Currency value={price} style={styles.price}/>
+          <Text>{moment(parseInt(estimatedDate, 10)).format('ddd Do MMMM, h:mma')}</Text>
+        </Col>
+        <Col size={23}>
+          <AcceptPartner disabled={!canRespond} busyUpdating={busyUpdating} style={{marginBottom: 10}} orderId={orderId} orderContentTypeId={orderContentTypeId} partnerId={partnerId}  dispatch={dispatch}/>
+          <RejectPartner disabled={!canRespond}  busyUpdating={busyUpdating} orderId={orderId} orderContentTypeId={orderContentTypeId} partnerId={partnerId} dispatch={dispatch}/>
+        </Col>
+      </Row>;
+    })}</Col>;
+};
+
+const styles = {
+  row: {
+    marginTop: 10,
+    marginBottom: 10
+  },
+  toggleStage: {
+    marginRight: 5,
+    justifyContent: 'center',
+    flex: 1
+  },
+  view: {
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 5
+  },
+  heading: {
+    fontSize: 16
+  },
+  subHeading: {
+    fontSize: 14,
+    fontWeight: 'bold'
+
+  },
+  buttonText: {
+    fontSize: 10
+  },
+  text: {
+    marginRight: 5
+  },
+  star: {
+    fontSize: 15,
+    padding: 2,
+    color: shotgun.gold,
+  },
+  partnerImage: {
+    aspectRatio: 1,
+    width: 80,
+    height: 80,
+  },
+  price: {
+    fontSize: 30,
+    lineHeight: 34,
+    fontWeight: 'bold'
+  },
+  smlprice: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
+};
+
---
+++ shotgun-client/app/customer/orders/CustomerStagedPaymentPanel.js
@@ -0,0 +1,226 @@
+import React, {Component} from 'react';
+import {Button, Text, Grid, Row, Col,  Item, Label, Spinner} from 'native-base';
+import {SpinnerButton, Currency, ValidatingInput, ValidatingButton, Icon} from 'common/components';
+import * as ContentTypes from 'common/constants/ContentTypes';
+import {addPaymentStage, removePaymentStage, payForPaymentStage} from 'customer/actions/CustomerActions';
+import shotgun from 'native-base-theme/variables/shotgun';
+import yup from 'yup';
+
+const CAN_ADD_PAYMENT_STAGE__ORDER_STATUSES = ['PLACED', 'ACCEPTED'];
+const CAN_MODIFY_PAYMENT_STAGE_STATUS = ['None'];
+const CAN_PAY_PAYMENT_STAGE_STATUS = ['Complete'];
+
+const validationSchemaBase = {
+  name: yup.string().required().max(30),
+  description: yup.string().required().max(30),
+};
+
+const percentageValidationSchema = () => ({
+  ...validationSchemaBase,
+  amount: yup.number().required().min(0).max(100)
+});
+
+const fixedPriceValidationSchema = orderAmount => ({
+  ...validationSchemaBase,
+  amount: yup.number().required().min(0).max(orderAmount)
+});
+
+const StageQuantity = ({quantity, paymentStageType, orderAmount}) => {
+  const isPercent = paymentStageType === 'Percentage';
+  const currencyTotal = isPercent ? orderAmount * (quantity / 100) : quantity;
+  return <Currency value={currencyTotal} style={styles.smlprice} suffix={isPercent ? ` (${quantity}%)` : ''}/>;
+};
+
+const  AddPaymentStage = (props) => {
+  const {orderId, orderContentTypeId, amount, orderAmount, name, description, paymentStageType, onPaymentStageAdded, busyUpdating, dispatch, ...rest} = props;
+  const validationSchemaFactory = paymentStageType === 'Percentage' ? percentageValidationSchema : fixedPriceValidationSchema;
+  const validationSchema = validationSchemaFactory(orderAmount);
+  const onAddPaymentStage = () => {
+    dispatch(addPaymentStage({orderId, orderContentTypeId, amount, name, description, paymentStageType}, onPaymentStageAdded));
+  };
+  return <ValidatingButton {...rest} busy={busyUpdating} fullWidth success onPress={onAddPaymentStage} validationSchema={yup.object(validationSchema)} model={props}>
+    <Text uppercase={false}>Add</Text>
+  </ValidatingButton>;
+};
+
+const  PayForStage = ({orderId, paymentStageId, onPaymentStageRemoved, orderContentTypeId, busyUpdating, dispatch, ...rest}) => {
+  const onUpdateOrderAmount = () => {
+    dispatch(payForPaymentStage({orderId, paymentStageId, orderContentTypeId}, onPaymentStageRemoved));
+  };
+  return <SpinnerButton {...rest} padded busy={busyUpdating} style={{alignSelf: 'flex-start', flex: 1, marginRight: 0, marginLeft: 0}} fullWidth success onPress={onUpdateOrderAmount}><Text uppercase={false}>Pay</Text></SpinnerButton>;
+};
+
+const  RemovePaymentStage = ({orderId, paymentStageId, onPaymentStageRemoved, orderContentTypeId, busyUpdating, dispatch, ...rest}) => {
+  const onUpdateOrderAmount = () => {
+    dispatch(removePaymentStage({orderId, paymentStageId, orderContentTypeId}, onPaymentStageRemoved));
+  };
+  return <SpinnerButton {...rest} padded busy={busyUpdating} style={{alignSelf: 'flex-start', flex: 1, marginRight: 0, marginLeft: 0}} danger fullWidth onPress={onUpdateOrderAmount}><Text uppercase={false}>Del</Text></SpinnerButton>;
+};
+
+class AddPaymentStageControl extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      amount: undefined,
+      name: undefined,
+      description: undefined
+    };
+  }
+
+  resetState = () => {
+    this.setState({
+      amount: undefined,
+      name: undefined,
+      description: undefined,
+      reset: true
+    });
+    if (this.nameInput){
+      this.nameInput.clearTouched();
+    }
+    if (this.descriptionInput){
+      this.descriptionInput.clearTouched();
+    }
+    if (this.amountInput){
+      this.amountInput.clearTouched();
+    }
+  }
+
+  render(){
+    const {paymentStageType, orderAmount} = this.props;
+    const validationSchemaFactory = paymentStageType === 'Percentage' ? percentageValidationSchema : fixedPriceValidationSchema;
+    const validationSchema = validationSchemaFactory(orderAmount);
+    return <Col>
+      <Item key="1" stackedLabel  style={{marginLeft: 4, marginRight: 10, flex: 1, marginBottom: 10}} >
+        <Label>Name</Label>
+        <ValidatingInput ref={rf => {this.nameInput = rf;}} bold style={{padding: 10}} padded value={this.state.name}placeholder="First fix electrics" onChangeText={(name) => this.setState({name})} validationSchema={validationSchema.name} maxLength={30}/>
+      </Item>
+      <Item key="2" stackedLabel  style={{marginLeft: 4, marginRight: 10, flex: 1, marginBottom: 10}} >
+        <Label>Description</Label>
+        <ValidatingInput ref={rf => {this.descriptionInput = rf;}} bold style={{padding: 10}} padded value={this.state.description} placeholder="Complete all first fix of bathroom" onChangeText={(description) => this.setState({description})} validationSchema={validationSchema.name} maxLength={30}/>
+      </Item>
+      <Item key="3" stackedLabel  style={{marginLeft: 4, marginRight: 10, flex: 1, marginBottom: 10}} >
+        <Label>{paymentStageType === 'Percentage' ?  'Percentage' : 'Amount'}</Label>
+        <ValidatingInput ref={rf => {this.amountInput = rf;}} bold style={{padding: 10}} value={this.state.amount} placeholder="25" onChangeText={(amount) => this.setState({amount})} validationSchema={validationSchema.amount} maxLength={10}/>
+      </Item>
+      <AddPaymentStage {...this.props} {...this.state} onPaymentStageAdded={this.resetState}/>
+    </Col>;
+  }
+}
+
+const PartnerPaymentStagesControl = ({paymentStages = [], orderId, orderStatus, orderContentTypeId, paymentStageType, busyUpdating, dispatch, orderAmount}) => {
+  return <Col>{paymentStages.map(
+    (paymentStage, idx)  => {
+      const {quantity, name, description, paymentStageType, paymentStageStatus, id, lastUpdated} = paymentStage;
+      return <Row key={idx} style={{marginBottom: 10, marginLeft: 3, width: '100%'}}>
+        <Col  size={50}>
+          <Text style={{...styles.subHeading, marginBottom: 5}}>{name}</Text>
+          <Text>{description}</Text>
+        </Col>
+        <Col size={15} style={{paddingTop: 10}}>
+          <StageQuantity quantity={quantity} paymentStageType={paymentStageType} orderAmount={orderAmount}/>
+        </Col>
+        <Col size={15} >
+          {!!~CAN_MODIFY_PAYMENT_STAGE_STATUS.indexOf(paymentStageStatus) && !!~CAN_ADD_PAYMENT_STAGE__ORDER_STATUSES.indexOf(orderStatus) ? <RemovePaymentStage busyUpdating={busyUpdating} style={{marginBottom: 10}} orderId={orderId} orderContentTypeId={orderContentTypeId} paymentStageId={id} dispatch={dispatch}/> : null}
+          {!!~CAN_PAY_PAYMENT_STAGE_STATUS.indexOf(paymentStageStatus) ? <PayForStage busyUpdating={busyUpdating} orderId={orderId} orderContentTypeId={orderContentTypeId} paymentStageId={id} dispatch={dispatch}/> : null}
+          {paymentStageStatus === 'Started' ?  <Spinner style={{height: 50, paddingRight: 15}}/> : null}
+          {paymentStageStatus === 'Paid' ? <Icon name='star' style={styles.star}/> : null}
+        </Col>
+      </Row>;
+    })}
+  {!!~CAN_ADD_PAYMENT_STAGE__ORDER_STATUSES.indexOf(orderStatus) ? <AddPaymentStageControl orderAmount={orderAmount} busyUpdating={busyUpdating} paymentStageType={paymentStageType} orderId={orderId} orderContentTypeId={orderContentTypeId} dispatch={dispatch}/> : null}
+  </Col>;
+};
+
+export default class OrderPaymentStagePanel extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      paymentStageType: 'Percentage'
+    };
+  }
+
+  setPaymentStageType = (paymentStageType) => {
+    this.setState({paymentStageType});
+  }
+
+  render(){
+    const {order, busyUpdating, dispatch} = this.props;
+    const {paymentStages = []} = order;
+    const canAddPaymentStages = !!~CAN_ADD_PAYMENT_STAGE__ORDER_STATUSES.indexOf(order.orderStatus);
+    if (!order){
+      return null;
+    }
+    return [<Grid  key="1" style={{paddingTop: 10, marginBottom: 10}}>
+      <Col  key="2" size={32}>
+        < Text style={{...styles.heading, marginTop: 10, marginBottom: 10}}>{canAddPaymentStages ? 'Add Payment Stages' : 'Payment Stages'} </Text>
+        {paymentStages.length ? null :
+          <Row>
+            <Button style={styles.toggleStage} light={this.state.paymentStageType === 'Fixed'} onPress={() => this.setPaymentStageType('Percentage')}>
+              <Text style={styles.buttonText}>Percentage Stages</Text>
+            </Button>
+            <Button style={styles.toggleStage} light={this.state.paymentStageType  === 'Percentage'} onPress={() => this.setPaymentStageType('Fixed')}>
+              <Text style={styles.buttonText}>Fixed Stages</Text>
+            </Button>
+          </Row>}
+      </Col>
+    </Grid>,
+    <PartnerPaymentStagesControl key="3"
+      paymentStageType={this.state.paymentStageType} dispatch={dispatch} orderId={order.orderId} orderStatus={order.orderStatus} orderAmount={order.amount} orderContentTypeId={order.orderContentTypeId} paymentStages={paymentStages} busyUpdating={busyUpdating}/>];
+  }
+}
+
+const styles = {
+  row: {
+    marginTop: 10,
+    marginBottom: 10
+  },
+  toggleStage: {
+    marginRight: 5,
+    justifyContent: 'center',
+    flex: 1
+  },
+  view: {
+    marginLeft: 30,
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 5
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  subHeading: {
+    fontSize: 14,
+    fontWeight: 'bold'
+
+  },
+  buttonText: {
+    fontSize: 10
+  },
+  text: {
+    marginRight: 5
+  },
+  star: {
+    paddingTop: 10,
+    alignSelf: 'center',
+    fontSize: 30,
+    padding: 2,
+    color: shotgun.brandSuccess,
+  },
+  partnerImage: {
+    aspectRatio: 1,
+    width: 80,
+    height: 80,
+  },
+  price: {
+    fontSize: 30,
+    lineHeight: 34,
+    fontWeight: 'bold'
+  },
+  smlprice: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  }
+};
