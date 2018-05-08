960854e99f6f6bdb217ac172c88ef760ccb30288
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-05-08 18:39:52 +0100

    Fixed things

--- shotgun-client/app/customer/orders/CustomerOrderDetail.js
+++ shotgun-client/app/customer/orders/CustomerOrderDetail.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
 import {connect, ReduxRouter, Route} from 'custom-redux';
 import {Container, Header, Left, Button, Body, Title, Content, Text, Tab, View, Spinner, Row} from 'native-base';
-import {Icon, LoadingScreen, ErrorRegion, SpinnerButton, OrderSummary, Tabs, RatingSummary, PriceSummary} from 'common/components';
+import {Icon, LoadingScreen, ErrorRegion, OrderSummary, Tabs, RatingSummary} from 'common/components';
 import {resetSubscriptionAction, getDaoState, isAnyOperationPending, getNavigationProps, getOperationErrors, getDao, getAnyOperationError} from 'common/dao';
 import * as ContentTypes from 'common/constants/ContentTypes';
 import OrderLifecycleView from 'common/components/orders/OrderLifecycleView';
-import {cancelOrder} from 'customer/actions/CustomerActions';
 import shotgun from 'native-base-theme/variables/shotgun';
 import CustomerNegotiationPanel from './placed/CustomerNegotiationPanel';
+import CustomerPriceSummary from './CustomerPriceSummary';
 import CustomerStagedPaymentPanel from './progress/CustomerStagedPaymentPanel';
 import CustomerHireOrderInProgress from './progress/CustomerHireOrderInProgress';
 import CustomerJourneyOrderInProgress from './progress/CustomerJourneyOrderInProgress';
 import CompleteControl from './progress/CompleteControl';
+import CancelControl from './progress/CancelControl';
 import VehicleDetails from './progress/VehicleDetails';
 import OrderSummaryDao from 'common/dao/OrderSummaryDao';
+
 const JourneyJobInProgress = (message) => ({order}) => ( order.journeyOrderStatus == 'ENROUTE' ? <Row  style={{paddingLeft: 25, paddingTop: 10, paddingBottom: 25}}><Spinner style={{height: 15, marginRight: 10}}/><Text>{message}</Text></Row> : null);
 class CustomerOrderDetail extends Component{
   constructor(props) {
@@ -39,7 +41,7 @@ class CustomerOrderDetail extends Component{
   }
 
   render() {
-    const {busy, order, orderId, errors, history, busyUpdating, dispatch, height, path} = this.props;
+    const {busy, order, orderId, errors, history} = this.props;
     const {resources} = this;
     const {InProgressControls} = resources;
     return busy || !order ? <LoadingScreen text={ !busy && !order ? 'Order "' + orderId + '" cannot be found' : 'Loading Order...'}/> : <Container>
@@ -59,8 +61,8 @@ class CustomerOrderDetail extends Component{
             PlacedControls={[CustomerNegotiationPanel, OrderSummary]}
             InProgressControls={[...InProgressControls]}
             AcceptedControls={[CustomerNegotiationPanel, ...InProgressControls]}
-            CompletedControls={[PriceSummary, RatingSummary, OrderSummary]}
-            CancelledControls={[PriceSummary, RatingSummary, OrderSummary]}
+            CompletedControls={[CustomerPriceSummary, RatingSummary, OrderSummary]}
+            CancelledControls={[CustomerPriceSummary, RatingSummary, OrderSummary]}
           />
         </View>
       </Content>
@@ -100,13 +102,6 @@ const mapStateToProps = (state, initialProps) => {
   };
 };
 
-const  CancelOrder = ({orderId, busyUpdating, dispatch, ...rest}) => {
-  const onCancelOrder = () => {
-    dispatch(cancelOrder({orderId}));
-  };
-  return <SpinnerButton  {...rest} disabledStyle={{opacity: 0.1}}  busy={busyUpdating} fullWidth danger onPress={onCancelOrder}><Text uppercase={false}>Cancel</Text></SpinnerButton>;
-};
-
 const PaymentStagesAndSummary = (props) => {
   const {history, path, orderId, height, order} = props;
   const shouldShowPaymentStagesTab = order.paymentType !== 'DAYRATE' || (order.paymentStages && order.paymentStages.length);
@@ -133,10 +128,10 @@ resourceDictionary.
     personell((order) => `${order.orderProduct.name} Job`).
     rubbish((order) => `${order.orderProduct.name} Rubbish Collection`).
   property('InProgressControls', [OrderSummary]).
-    personell([PriceSummary, CompleteControl,PaymentStagesAndSummary/*, PersonellCustomerOrderInProgress*/]).
-    hire([PriceSummary,CompleteControl, CustomerHireOrderInProgress, OrderSummary]).
-    delivery([PriceSummary, JourneyJobInProgress('Delivery In Progress'), CompleteControl, CustomerJourneyOrderInProgress,VehicleDetails, props => <OrderSummary hideMap={true} {...props}/>]).
-    rubbish([PriceSummary, JourneyJobInProgress('Collection In Progress'),CompleteControl ,CustomerJourneyOrderInProgress, VehicleDetails, props => <OrderSummary hideMap={true} {...props}/>])
+    personell([CustomerPriceSummary, CompleteControl, CancelControl, PaymentStagesAndSummary/*, PersonellCustomerOrderInProgress*/]).
+    hire([CustomerPriceSummary,CompleteControl,CancelControl, CustomerHireOrderInProgress, OrderSummary]).
+    delivery([CustomerPriceSummary, JourneyJobInProgress('Delivery In Progress'), CompleteControl,CancelControl, CustomerJourneyOrderInProgress,VehicleDetails, props => <OrderSummary hideMap={true} {...props}/>]).
+    rubbish([CustomerPriceSummary, JourneyJobInProgress('Collection In Progress'),CompleteControl ,CancelControl, CustomerJourneyOrderInProgress, VehicleDetails, props => <OrderSummary hideMap={true} {...props}/>])
 /*eslint-enable */
 
 export default connect(
