829ada100496683fc278d2fe65d1ef748f23a48e
Gbemiga Meduoye <gmeduoye@gmail.com>
2018-05-14 19:12:48 +0100

    Fixed customer menu bar

--- shotgun-client/app/customer/CustomerMenuBar.js
+++ shotgun-client/app/customer/CustomerMenuBar.js
@@ -6,7 +6,7 @@ import {Icon} from 'common/components';
 const CustomerMenuBar = ({history, path}) => {
   return <Footer>
     <FooterTab><Button transparent onPress={() => history.just({pathname: `${path}/Checkout`})}><Icon name='list'/></Button></FooterTab>
-    <FooterTab><Button transparent onPress={() => history.just({pathname: `${path}/CustomerOrders`})}><Icon name='jobs'/></Button></FooterTab>
+    <FooterTab><Button transparent onPress={() => history.just({pathname: `${path}/CustomerMyOrders`})}><Icon name='jobs'/></Button></FooterTab>
     <FooterTab><Button transparent onPress={() => history.just({pathname: `${path}/UserRelationships`})}><Icon style={{marginLeft: 10, marginRight: 10}} name='one-person'/></Button></FooterTab>
     <FooterTab><Button transparent onPress={() => history.just({pathname: `${path}/Settings`})}><Icon name='cog'/></Button></FooterTab>
   </Footer>;
