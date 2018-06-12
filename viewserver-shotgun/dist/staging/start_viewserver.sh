383202f9c343c9cf907185b008653c1c511a7c06
Paul Graves <paulgraves@shotgun.ltd>
2018-06-12 17:13:55 +0100

    Trying to fix logging

--- viewserver-shotgun/dist/staging/start_viewserver.sh
+++ viewserver-shotgun/dist/staging/start_viewserver.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 cd /usr/bin/shotgun-viewserver
-nohup java -jar  -Xmx250m -Xms250m -XX:StringTableSize=1000003 -Dlog4j.configurationFile=log4j/log4j2-staging.xml -Dshotgun.environment=staging viewserver-shotgun-*.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar  -Xmx250m -Xms250m -XX:StringTableSize=1000003 -Dlog4j.configurationFile=log4j/log4j2-prod.xml -Dshotgun.environment=staging viewserver-shotgun-*.jar > /dev/null 2> /dev/null < /dev/null &
