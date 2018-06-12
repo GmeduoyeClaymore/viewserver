6edc9baf65e583b824be12ffedb3e3d9d357e31b
Paul Graves <paulgraves@shotgun.ltd>
2018-06-12 13:27:47 +0100

    Fixed prod and preprod scripts

--- viewserver-shotgun/dist/prod/start_viewserver.sh
+++ viewserver-shotgun/dist/prod/start_viewserver.sh
@@ -1,4 +1,3 @@
 #/bin/bash
 cd /usr/bin/shotgun-viewserver
-echo pwd
-nohup java -jar  -Xmx250m -Xms250m -XX:StringTableSize=1000003 -Dlog4j.configurationFile=log4j/log4j2-pre-prod.xml -Dshotgun.environment=prod viewserver-shotgun-*.jar $1 &
+nohup java -jar  -Xmx250m -Xms250m -XX:StringTableSize=1000003 -Dlog4j.configurationFile=log4j/log4j2-pre-prod.xml -Dshotgun.environment=prod viewserver-shotgun-*.jar> /dev/null 2> /dev/null < /dev/null &
