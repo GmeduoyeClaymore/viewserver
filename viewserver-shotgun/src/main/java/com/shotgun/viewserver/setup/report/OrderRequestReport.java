a80d7a535eefad6a7ba5c60d18c0a79ad5efe45f
Paul Graves <paulgraves@shotgun.ltd>
2018-06-25 15:15:29 +0100

    Removed extra column to fix build

--- viewserver-shotgun/src/main/java/com/shotgun/viewserver/setup/report/OrderRequestReport.java
+++ viewserver-shotgun/src/main/java/com/shotgun/viewserver/setup/report/OrderRequestReport.java
@@ -69,7 +69,6 @@ public class OrderRequestReport {
                                                 new IProjectionConfig.ProjectionColumn("orderDetails"),
                                                 new IProjectionConfig.ProjectionColumn("requiredDate"),
                                                 new IProjectionConfig.ProjectionColumn("lastModified"),
-                                                new IProjectionConfig.ProjectionColumn("created"),
                                                 new IProjectionConfig.ProjectionColumn("orderId"),
                                                 new IProjectionConfig.ProjectionColumn("productName"),
                                                 new IProjectionConfig.ProjectionColumn("path"),
