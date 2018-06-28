aed2499215066dcd4edaaf3aef61a90bbb2d0cc8
Gbemiga Meduoye <gbemiga@claymoreminds.com>
2018-06-28 23:05:01 +0100

    Shotgun domain is not required

--- viewserver-shotgun/dist/start_viewserver.sh
+++ viewserver-shotgun/dist/start_viewserver.sh
@@ -17,8 +17,5 @@ if [[ -z "$SHOTGUN_ISMASTER" ]]; then
     echo "Environment variable SHOTGUN_ISMASTER is required and has not been set on this host"
    exit 1
 fi
-if [[ -z "$SHOTGUN_DOMAIN" ]]; then
-    echo "Environment variable SHOTGUN_DOMAIN is required and has not been set on this host"
-   exit 1
-fi
+
 nohup java -jar  -Xmx250m -Xms250m -XX:StringTableSize=1000003 -Dlog4j.configurationFile=log4j/log4j2-$SHOTGUN_ENVIRONMENT_NAME.xml -Dserver.isMaster=$SHOTGUN_DOMAIN -Dserver.isMaster=$SHOTGUN_ISMASTER -Dserver.endpoint=$SHOTGUN_ENDPOINT -Dshotgun.environment=$SHOTGUN_ENVIRONMENT_NAME viewserver-shotgun-*.jar > /dev/null 2> /dev/null < /dev/null &
